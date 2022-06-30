0f843365ee1655acc01dcdb4505eb7efed4a4c45
MysterBox <yoganbelajar4@gmail.com>
2022-06-30 12:18:33 +0800

    Belajar If-else dan tenery Operator

--- NodeJs/coba.js
+++ NodeJs/coba.js
@@ -1,23 +1,30 @@
-let language = 'Bali';
-let salamPembuka = null
+// let result = 81;
+//  let score = (result>90) ? 'A' : 'B';
+//      score = (result>=80 && result<=89) ? 'B' : 'C';
+//      score = (result>=70 && result<=79) ? 'C' : 'D';
+//      score = (result>=60 && result<=69) ? 'D' : 'E';
+//      score = (result<=60 && result>=0 ) ? 'E' : 'tidak lulus';
 
-switch(language){
-    case 'Bali':
-        salamPembuka = 'semeton';
-        break;
+//  console.log(score);
 
-    case 'jakarta':
-        salamPembuka = 'Oi Kontol';
-        break;
-
-    case 'surabaya':
-        salamPembuka = 'Halo Siak'
-        break;
+// let nilai = -1;
+//  let score = nilai >90 ? 'A'
+//            : nilai >=80 && nilai <=89 ? 'B'
+//            : nilai >=70 && nilai <=79 ? 'C'
+//            : nilai >=60 && nilai <=69 ? 'D'
+//            : nilai >=0  && nilai <=59 ? 'E'
+//            : console.log('loh');
         
-    default:
-        salamPembuka = 'Tidak Ada salam Apapun';
-}
-console.log(salamPembuka);
-/* Keterangan
-kalau di Switch-case tidak ada break nya maka jawabannya'
-akan di majukan sampai bertemu break atau bertemu default*/
+//            console.log(score);
+
+let nilai=76;
+
+if (score>=90){
+console.log(result = 'Anda mendapatkan nilai A.');
+}else if (score >=80 && score <=89){
+console.log(result = 'Anda mendapatkan nilai B.');
+}else if (score >=70 && score <=79){
+console.log(result = 'Anda mendapatkan nilai C.');    
+}else if (score >=60 && score <=69){
+console.log(result = 'Anda mendapatkan nilai D.');    
+}else {(result = 'Anda mendapatkan nilai E.');}
