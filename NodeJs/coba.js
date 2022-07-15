538d2be68581f068a54499949037d40998471941
MysterBox <yoganbelajar4@gmail.com>
2022-07-15 21:46:31 +0800

    belajar Function (1) NodeJs

--- NodeJs/coba.js
+++ NodeJs/coba.js
@@ -1,39 +1,86 @@
-const mySet1 = new Set()
+// Belajar Function
+// ini cara Declarisi Fuction
+function coba1(){
+	console.log('Hello World !');
+}
+coba1();
+// tetapi ketika function menjalan kan baris code yang sama kurang fucsional
+// di sini kita dapan membuat function menjadi lebih fucsional menggunakan parameter
 
-mySet1.add(1)           // Set [ 1 ]
-mySet1.add(5)           // Set [ 1, 5 ]
-mySet1.add(5)           // Set [ 1, 5 ]
-mySet1.add('some text') // Set [ 1, 5, 'some text' ]
-const o = {a: 1, b: 2}
-mySet1.add(o)
-mySet1.add({a: 1, b: 2})   // o is referencing a different object, so this is okay
+// di sini contoh kita memanfaatkan parameter untuk mengubah perilaku function
+function siapaAku(name){ // ini menggunakan parameter string
+	if(name === "yogan"){
+		console.log('kamu benar');
+	}else{
+		console.log('kamu salah');
+	}
+}
+siapaAku('yogan');
+// ada perbedaan parameter string dan parameter number
+// kalau  di string kita bisa juga tidak pakai retrun
+// sedang kan kalau number kita harus menggunakan return untuk menggeluarkan hasil dari function
 
-mySet1.has(1)              // true
-mySet1.has(3)              // false, since 3 has not been added to the set
-mySet1.has(5)              // true
-console.log(mySet1.has(Math.sqrt(25)));  // true
-mySet1.has('Some Text'.toLowerCase()) // true
-mySet1.has(o)       // true
+// ini contoh parametern number dengan menggunakan return
+function cobaNum(i,j){
+	let result = i*j;
+	return result
+}
+console.log(cobaNum(5,5));
 
-mySet1.size         // 5
+// ini contoh penggunaan return pada string
+function akuSiapa(name){
+	if(name === 'yogan'){
+		return console.log('kamu bener benar dukun');
+	}else if(name === 'yogan tara'){
+		return console.log('kamu hampir benar');
+	}else{
+		return console.log('kamu goblok');
+	}
+}
+akuSiapa('yogan tara');
 
-mySet1.delete(5)    // removes 5 from the set
-mySet1.has(5)       // false, 5 has been removed
+// kita juga bisa memakai greeting() === bisa apa saja namanya bukan greeting saja 
+// agar kita bisa mengubah ngubah nama function parameternya
+// function greeting(i,j){
+// 	let result2 = i+j;
+// 	return console.log(result2);
+// }
+// let pertambahan = greeting(5,5);
+// pertambahan;
+// !!! kita tidak bisa menggunakan satu nama ke banyak parameter
+function greeting(a,b){
+	 let result = a*b;
+	 return result;
+}
+let perkalian = greeting(5,5);
+console.log(perkalian);
 
-mySet1.size         // 4, since we just removed one value
+// entah kenapa function greeting masih ada bag
 
-console.log(mySet1)
+// ada juga nama nya Expression Function
+// Pada expression function umumnya kita tidak perlu menuliskan nama fungsinya. 
+// Fungsi yang tidak bernama juga dikenal dengan anonymous function. 
+// Berikut ini merupakan contoh penulisan expression function:
 
-// fuction dari Math.sqrt adalah mencari kebenaran dari akar kuadrat
-console.log(Math.sqrt(49));
+const greeti = function(name, language) {
+    if(language === "English") {
+        return "Good Morning " + name + "!";
+    } else if (language === "French") {
+        return "Bonjour " + name + "!";
+    } else {
+        return "Selamat Pagi " + name + "!";
+    }
+}
+let foo = console.log(greeti('Ron', 'English'));
 
-// Structur data Set tidak bisa di duplikasi tapi set bisa di duplikasi
-// ketika mengacu pada object yang berbeda
-let set1 = new Set();
-set1.add(1);
-set1.add(2);
-set1.add(3);
-const a = {a:2}
-set1.add(a);
-set1.add({a:2});
-console.log(set1);
+const greet = function(name, negara){
+	if(negara === 'indonesia' === 'Indonesia'){
+		return 'selamat pagi ' + name;
+	}else if(negara === 'bali' === 'Bali'){
+		return 'cicing cai ' + name; 
+	}else {
+		return 'dasar bukan ' + name +' golongan kami';
+	}
+}
+console.log(greet('yogan','bali'));
+
