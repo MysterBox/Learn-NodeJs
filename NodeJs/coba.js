2a07a770394b3bdc2900a61e0315a9cae36965f5
MysterBox <yoganbelajar4@gmail.com>
2022-07-20 15:07:20 +0800

    Belajar Arrow Function NodeJs

--- NodeJs/coba.js
+++ NodeJs/coba.js
@@ -1,64 +1,44 @@
-// Belajar Function Parameter
-//kita bisa memanfaatkan destructuring object untuk memebuat parameter agar menghasilkan nilai
-const object1 = {
-    namaDepan: 'yogan',
-    namaBelakang: 'tara',
+// Belajar Arrow Function / Arrow Function Expresion
+
+// Ini Funciton Reguler
+// function declaration
+function sayHello(a,b) {
+    console.log(a*b);
 }
-// kalau kita tidak memaki tanda kurung sepesial object tidak akan keluar
-function namaLengkap({namaDepan,namaBelakang}){
-console.log(`${namaDepan} ${namaBelakang}`);
+ sayHello(4,4);
+
+// function expression
+const sayName = function (name) {
+    console.log(`Nama saya ${name}`)
 }
-namaLengkap(object1)
+sayName('ahCok');
 
-// DEFAULT PARAMETER
-// Fungsi pada JavaScript tidak melakukan pengecekan terhadap jumlah maupun tipe 
-// argumen yang dimasukkan ke dalam parameter.Ini berarti kita bisa
-// memasukkan argumen meskipun tidak sesuai dengan parameter yang telah didefinisikan.
-function exponentsFormula1(baseNumber, exponent) {
-    let result = baseNumber ** exponent;
-    console.log(`${baseNumber}^${exponent} = ${result}`);
+// Ini Function Arrow
+// function expression
+const sayHello2 = (greet) => {
+    console.log(`${greet}!`)
 }
-
-exponentsFormula1(2);
-/* output
-2^undefined = NaN
-*/
-
-// Seperti yang kita lihat pada contoh kode di atas, ketika argumen lebih sedikit dari parameternya,
-// maka parameter yang tidak terdefinisi akan bernilai undefined. Sebagai solusi jika memungkinkan,
-// kita bisa memberikan nilai default pada parameter. Nilai ini akan digunakan jika kita tidak memasukkan parameter.
-
-function exponentsFormula2(baseNumber, exponent = 3) {
-    let result = baseNumber ** exponent;
-    console.log(`${baseNumber}^${exponent} = ${result}`);
+sayHello2('yogan');
+ 
+const sayName2 = (name) => {
+    console.log(`Nama saya ${name}`)
 }
+sayName2('Tara');
 
-exponentsFormula2(3);
-/* output
-3^2 = 9
-*/
+// Perbedaan Funciton Reguler dan Arrow adalah 
+// Function Reguler bisa memakai Function Expression Dan Function Declaration
+// sedangkan Function Arrow hanya memiliki Function Expression saja
 
-// Rest Parameter
-// Rest parameter juga dituliskan menggunakan three consecutive dots (...).
-//  Dengan rest parameter, kita dapat menggabungkan beberapa elemen menjadi satu array.
-//   Tentu teknik ini sangat bermanfaat ketika kita hendak membuat sebuah fungsi dengan parameter yang tidak pasti.
-function sum(...numbers) {
-    let result = 0;
-    for (let number of numbers) {
-        result += number;
-    }
-    return result;
-}
+// Apabila fungsi hanya memiliki satu parameter, maka kita bisa menghapuskan tanda kurung seperti
+const hello = name => {console.log(`Hello ${name}`);}
+hello('World');
 
-console.log(sum(1, 2, 3, 4, 5));
 
-function sum2(...number){
-    result = 0;
-    for (const simpan of number) {
-        result += simpan;
-    }
-    return console.log(result);
-}
-
-sum2(1,2,3,4,5,6);
-
+// kita juga menulis nya dengan lebih singakat lagi 
+// dan ketika gita tidak memerlukan parameter kita harus menuliskan tanda kurung kosong saja
+const greet = () => console.log('Selamat Pagi Semua');
+greet();
+// dan jika kita menuliskan kode seperti di atas 
+// dan ingin mengembalika hasil yang di berikan kita bisa tidak menuliskan return pada code
+const perkalian = (a,b) => console.log(a*b);
+perkalian(5,5);
